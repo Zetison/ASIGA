cb8bf36e671e9e195d1f2924feffe51ef52157a5
Zetison <jonvegard89@yahoo.com>
2019-04-23 16:48:45 +0200

    1.41

--- subRoutines/getTask_Simpson_Torus.m
+++ subRoutines/getTask_Simpson_Torus.m
@@ -9,10 +9,10 @@ formulation = {'CCBIE'};
 k = 2;
 % f = 1e2;             % Frequency
 f = k*1500/(2*pi);
-M = 1:4;
+M = 1:5;
 % M = 3; 
 degree = 2:5;
-degree = 2;
+% degree = 3;
 calculateSurfaceError = 1;
 calculateFarFieldPattern = 0;
 % extraGPBEM = [0,3];
@@ -27,7 +27,6 @@ exteriorProblem = false;
 % agpBEM = [1,2,4,8]; % parameter for adaptiv Gauss point integration around singularities for BEM formulations
 agpBEM = 2; % parameter for adaptiv Gauss point integration around singularities for BEM formulations
 useNeumanProj = [1,0];
-useNeumanProj = 1;
 computeCondNumber = true;
 
 loopParameters = {'M','degree','method','formulation','extraGP','extraGPBEM','agpBEM','useNeumanProj'};
@@ -45,6 +44,4 @@ agpBEM = 2; % parameter for adaptiv Gauss point integration around singularities
 M = 1:7;
 method = {'BA'};
 formulation = {'SL2E'};
-% collectIntoTasks
-
-
+collectIntoTasks
