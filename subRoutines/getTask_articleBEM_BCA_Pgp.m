6fae39bc68672ee5ccd08587251e2d60a4beaa18
Zetison <jonvegard89@yahoo.com>
2019-06-08 08:57:42 +0200

    1.138

--- subRoutines/getTask_articleBEM_BCA_Pgp.m
+++ subRoutines/getTask_articleBEM_BCA_Pgp.m
@@ -27,12 +27,17 @@ storeSolution = false;
 storeFullVarCol = false;
 loopParameters = {'method','degree','extraGPBEM','formulation','agpBEM'};
 degree = [2,5];
-degree = 5;
+% degree = 5;
 
 runTasksInParallel = false; % extra quadrature points
 extraGP = 0; % extra quadrature points
-extraGPBEM = 128; % extra quadrature points
+extraGPBEM = 256; % extra quadrature points
 agpBEM = [0.2,0.6,1,1.4,1.8,2.2,2.6];
+collectIntoTasks
+
+
+extraGPBEM = [8,16,32,64,128,256,512];
+agpBEM = 1.4;
 collectIntoTasks
 
 agpBEM = agpBEM([1,end]);
--- subRoutines/getTask_articleBEM_S1_qp.m
+++ subRoutines/getTask_articleBEM_S1_qp.m
@@ -31,7 +31,7 @@ plot2Dgeometry = 0;
 plot3Dgeometry = 0;
 
 extraGP = 0; % extra quadrature points
-extraGPBEM = [0,4,8,16,32,64]; % extra quadrature points
+extraGPBEM = [0,4,8,16,32]; % extra quadrature points
 agpBEM = 0:0.5:1.5;
 % agpBEM = 3;
 colBEM_C0 = 0;
@@ -40,9 +40,16 @@ quadMethodBEM = {'Simpson'};
 loopParameters = {'extraGPBEM','agpBEM','M','degree','formulation','method','quadMethodBEM'};
 % collectIntoTasks
 
-agpBEM = 0.2:0.4:1.4;
+agpBEM = 0.2:0.1:0.7;
+extraGPBEM = 32; % extra quadrature points
 quadMethodBEM = {'Adaptive'};
 collectIntoTasks
+
+
+extraGPBEM = [0,1,2,4,8,16,32]; % extra quadrature points
+agpBEM = 0.7;
+collectIntoTasks
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 method = {'BA'};
 extraGPBEM = NaN;
