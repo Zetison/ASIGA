04c543940b06408a4c7e26d018be9d9cdf62be79
Zetison <jonvegard89@yahoo.com>
2019-06-14 09:55:34 +0200

    1.149

---
+++ subRoutines/getTask_articleBEM_BCA_paraview.m
@@ -0,0 +1,34 @@
+scatteringCase = {'BI'}; % 'BI' = Bistatic scattering, 'MS' = Monostatic scattering
+
+model = 'BCA'; % BeTSSi submarine
+coreMethod = 'IGA';
+
+% alpha_s = 270*pi/180;
+alpha_s = 240*pi/180;
+beta_s = 0*pi/180;        
+plot3Dgeometry = 0;
+plot2Dgeometry = 0;  % Plot cross section of mesh and geometr
+
+alpha = (0:0.05:360)*pi/180;
+
+BC = 'SHBC';
+
+method = {'BEM'};
+formulation = 'CCBIE';
+loopParameters = {'method','formulation','M','degree','f','formulation','scatteringCase'};
+
+
+f = 1e3;             % Frequency
+M = 3;
+degree = 5;
+plotResultsInParaview = 1;
+plotMesh              = 1;	% Create additional Paraview files to visualize IGA mesh
+calculateSurfaceError = 0;
+calculateFarFieldPattern = 0;
+plotTimeOscillation   = 1;	% Create 30 paraview files in order to visualize a dynamic result
+computeCondNumber = 0;
+storeSolution = 0;
+storeFullVarCol = 0;
+loopParameters = {'method','degree'};
+collectIntoTasks
+
