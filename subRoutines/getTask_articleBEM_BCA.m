77db0a8d6fece0f6e5efcfd9c3782ec1321568d7
Zetison <jonvegard89@yahoo.com>
2019-06-16 10:53:49 +0200

    1.152

--- subRoutines/getTask_articleBEM_BCA.m
+++ subRoutines/getTask_articleBEM_BCA.m
@@ -20,7 +20,7 @@ BC = 'SHBC';
 
 method = {'BEM'};
 formulation = {'CCBIE'};
-M = 1:2;
+M = 1:3;
 degree = [2,5];
 storeSolution = 0;
 storeFullVarCol = 0;
@@ -28,37 +28,15 @@ loopParameters = {'method','formulation','M','degree','f','scatteringCase'};
 
 collectIntoTasks
 
-
-f = 1e2;             % Frequency
-M = 3;
-collectIntoTasks
-
-f = 1e3;             % Frequency
-M = 3;
-degree = 5;
-plotResultsInParaview = 1;
-plotMesh              = 1;	% Create additional Paraview files to visualize IGA mesh
-calculateSurfaceError = 0;
-calculateFarFieldPattern = 0;
-plotTimeOscillation   = 1;	% Create 30 paraview files in order to visualize a dynamic result
-computeCondNumber = 0;
-storeSolution = 0;
-storeFullVarCol = 0;
-loopParameters = {'method','degree'};
-collectIntoTasks
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 f = [1e2 1e3];
-plotResultsInParaview = 0;
-plotMesh              = 0;	% Create additional Paraview files to visualize IGA mesh
-plotTimeOscillation   = 0;	% Create 30 paraview files in order to visualize a dynamic result
 calculateSurfaceError = 1;
 applyLoad = 'radialPulsation';
 BC = 'NBC';
 M = 1:3;
 degree = [2,5];
 method = {'BA'};
-formulation = 'SL2E';
+formulation = {'SL2E'};
 collectIntoTasks
 
