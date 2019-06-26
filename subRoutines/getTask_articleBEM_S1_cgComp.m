ca5ce5705eaf62a23a42af7bd69a60bff393b457
Zetison <jonvegard89@yahoo.com>
2019-06-26 11:34:34 +0200

    1.172

--- subRoutines/getTask_articleBEM_S1_cgComp.m
+++ subRoutines/getTask_articleBEM_S1_cgComp.m
@@ -10,20 +10,24 @@ c_f = 1500;
 k = 1;
 omega = k*c_f;
 f = omega/(2*pi); 
-M = 1:6;
-parm = [0,1];
+M = 1:7;
+% M = 6;
+parm = [1,2];
+parm = 1;
 
 alpha_s = 240*pi/180;
 beta_s = 30*pi/180;
 solveForPtot = true;
 
 colMethod = {'CG','Grev','GL'};
+colMethod = {'Grev'};
 degree = 2:5;
+% degree = 3;
 calculateFarFieldPattern = 0;
 calculateSurfaceError = 1;
 plot2Dgeometry = 0;
 plot3Dgeometry = 0;
-loopParameters = {'colMethod','M','parm','method','formulation','degree'};
+loopParameters = {'colMethod','M','method','formulation','degree','parm'};
 % agpBEM = 2;
 colBEM_C0 = 0;
 quadMethodBEM = 'Adaptive';
