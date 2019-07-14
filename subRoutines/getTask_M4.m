faa5a9c227dc342ec59584985bed57a8645cc7db
Zetison <jonvegard89@yahoo.com>
2019-07-14 20:22:35 +0200

    1.205

--- subRoutines/getTask_M4.m
+++ subRoutines/getTask_M4.m
@@ -6,16 +6,15 @@ model = 'M4'; % BeTSSi model 5A and BeTSSi model 5B
 
 method = {'BEM'};
 formulation = {'CBM','CCBIE'};
-formulation = {'CCBIE'};
+% formulation = {'CCBIE'};
 
-f = [1e3, 3e3, 10e3];             % Frequency
-f = 1e3;             % Frequency
+f = 10e3;             % Frequency
 
 M = 3:6;
-M = 3;
+M = 7;
 parm = [2,1];
 % parm = 2;
-degree = 2;
+degree = 4;
 beta_s = 30*pi/180;
 alpha = (0:0.05:360)*pi/180;
 
