2f62fe7f698a026088a14363e97073cc4c209431
Zetison <jonvegard89@yahoo.com>
2020-08-27 23:55:15 +0200

    Fix redundant print

--- subRoutines/getTasks.m
+++ subRoutines/getTasks.m
@@ -3,6 +3,6 @@ function studiesCol = getTasks(studyName)
 studiesCol = cell(1,numel(studyName));
 
 for i_coll = 1:numel(studyName)
-    eval(['studiesCol{i_coll} = getTask_' studyName{i_coll}])
+    eval(['studiesCol{i_coll} = getTask_' studyName{i_coll} ';'])
 end
 
