5481d799786287d8c21f4e742dd44fd194281365
Jon Vegard Venås <jonvegard89@yahoo.com>
2021-03-25 23:50:19 +0100

    Fixed bug

--- examples/taskFiles/tests/getTask_Venas2018iao_Table2.m
+++ examples/taskFiles/tests/getTask_Venas2018iao_Table2.m
@@ -8,7 +8,7 @@ studies = cell(0,1);
 getDefaultTaskValues
 
 if nargin < 1
-    M_0 = 4; % 4
+    M_0 = 1; % 4
 end
 
 prePlot.plot2Dgeometry = 0; 
@@ -36,10 +36,8 @@ postPlot(1).xLoopName     	= 'msh.M';
 postPlot(1).fileDataHeaderX	= [];
 postPlot(1).noXLoopPrms   	= 0;
 postPlot(1).xScale          = 180/pi;
-% postPlot(1).addCommands  = @(study,i_study,studies) addCommands_(i_study,studies);
-postPlot(2) = postPlot(1);
-postPlot(2).yname = 'error_p';
-postPlot(2).axisType = 'semilogy';
+postPlot(1).addCommands  = @(study,i_study,studies) addCommands_(i_study,studies);
+
 warning('off','NURBS:weights')
 for i_coreM = 1:length(misc.coreMethods)
     misc.coreMethod = misc.coreMethods(i_coreM);
@@ -102,35 +100,47 @@ for i_coreM = 1:length(misc.coreMethods)
 end
 
 function addCommands_(i_study,studies)
-% close all
-if i_study == 1
-    for j = 1:3
-        tasks = studies(9+j).tasks;
-        tasks(end+1) = studies(6+j).tasks;
-        tasks(end+1) = studies(3+j).tasks(1);
-        tasks(end+1) = studies(j).tasks(1);
-        tasks(end+1) = studies(3+j).tasks(2);
-        studies2(j).tasks = tasks;
-    end
-    for j = 1:3
-        tasks = studies2(j).tasks;
-        for ii = 1:numel(tasks)
-            task = tasks(ii).task;
-            M = task.M;
-            degree = task.degree;
-            t_sys = task.varCol{1}.timeBuildSystem;
-            t_sol = task.varCol{1}.tot_time - t_sys;
-            noElems = task.varCol{1}.totNoElems;
-            dofs = task.dofs;
-            switch task.misc.coreMethod
-                case 'IGA'
-                    fprintf('Mesh ${\\cal M}_{%d,%d,%d}^{\\textsc{iga}}$\t\t\t & %6d\t & %6d \t & %5.2f & %6.2f & %5.2f \\cr\n', ...
+printForLaTeX = 0;
+for j = 1:3
+    tasks = studies(9+j).tasks;
+    tasks(end+1) = studies(6+j).tasks;
+    tasks(end+1) = studies(3+j).tasks(1);
+    tasks(end+1) = studies(j).tasks(1);
+    tasks(end+1) = studies(3+j).tasks(2);
+    studies2(j).tasks = tasks;
+end
+if ~printForLaTeX
+    fprintf('%-20s\t  %6s\t  %6s \t  %5s  %6s  %5s\n', ...
+             ' ', 'noElems', 'dofs', 't_sys', 't_sol', 'energyError');
+end
+for j = 1:3
+    tasks = studies2(j).tasks;
+    for ii = 1:numel(tasks)
+        task = tasks(ii).task;
+        M = task.msh.M;
+        degree = task.msh.degree;
+        t_sys = task.varCol{1}.timeBuildSystem;
+        t_sol = task.varCol{1}.tot_time - t_sys;
+        noElems = task.varCol{1}.totNoElems;
+        dofs = task.dofs;
+        switch task.misc.coreMethod
+            case 'IGA'
+                if printForLaTeX
+                    fprintf('Mesh ${\\cal M}_{%d,%d,%d}^{\\textsc{iga}}$\t\t & %6d\t & %6d \t & %5.2f & %6.2f & %5.2f \\cr\n', ...
                              M, degree, degree-1, noElems, dofs, t_sys, t_sol, task.results.energyError);
-                otherwise
+                else
+                    fprintf('Mesh M_{%d,%d,%d}^iga\t  %6d\t  %6d \t  %5.2f  %6.2f  %5.2f\n', ...
+                             M, degree, degree-1, noElems, dofs, t_sys, t_sol, task.results.energyError);
+                end
+            otherwise
+                if printForLaTeX
                     fprintf('Mesh ${\\cal M}_{%d,%d,\\mathrm{i}}^{\\textsc{fem}}$\t & %6d\t & %6d \t & %5.2f & %6.2f & %5.2f \\cr\n', ...
                              M, degree, noElems, dofs, t_sys, t_sol, task.results.energyError);
-            end
+                else
+                    fprintf('Mesh M_{%d,%d,i}^fem\t  %6d\t  %6d \t  %5.2f  %6.2f  %5.2f\n', ...
+                             M, degree, noElems, dofs, t_sys, t_sol, task.results.energyError);
+                end
         end
-        fprintf('\n')
     end
+    fprintf('\n')
 end
