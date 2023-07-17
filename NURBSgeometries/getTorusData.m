23d2d41dbab4289c84433a06939d6f56627aee73
Jon Vegard Venås <jonvegard89@yahoo.com>
2023-07-17 14:56:49 +0200

    Fixed g2-files

--- NURBSgeometries/getTorusData.m
+++ NURBSgeometries/getTorusData.m
@@ -3,6 +3,7 @@ options = struct('Xi', [0,0,0,1,1,2,2,3,3,3]/3, ...    % knot vector of revoluti
                  'Eta', [0,0,0,1,1,2,2,3,3,3]/3, ...   % knot vector of revolution in the eta-direction
                  'R', 2,...
                  'r', 1,...
+                 't',0,...
                  'parm', 1,...
                  'theta', 2*pi, ...   % angle of revolution in the xi direction
                  'theta_eta', 2*pi);     % angle of revolution in the eta direction 
@@ -15,9 +16,12 @@ if nargin > 0
     options = updateOptions(options,newOptions);
 end
 diskOptions = options;
-diskOptions.R = options.r;
+diskOptions.R = [options.r,options.r-options.t];
 nurbs = getDiskData(diskOptions);
 nurbs = translateNURBS(nurbs,[-options.R,0,0]);
 nurbs = rotateNURBS(nurbs,'theta',pi/2,'rotAxis','Xaxis');
 nurbs = revolveNURBS(nurbs,'Xi',options.Eta,'theta',options.theta_eta);
 nurbs = permuteNURBS(nurbs,[2,3,1]);
+if options.t == 0
+    nurbs = subNURBS(nurbs,'at',[0,0;0,0;0,1]);
+end
